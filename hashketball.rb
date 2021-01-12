a86c610dddb087e5820303624b41f2f466f8071b
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-12 19:46:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,7 +116,7 @@ def game_hash
           player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
-          points: 6,
+          points: 33,#6,
           rebounds: 12,
           assists: 12,
           steals: 7,
