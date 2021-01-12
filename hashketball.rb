7bb1eae90428685f7ee1443bae21cf1b06003535
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-12 19:49:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,8 +225,9 @@ def most_points_scored
   if high_scorers.length = 1
     name = high_scorers[:player_name]
   elsif high_scorers > 1
-    name += 
-  binding.pry
+    name += high_scorers.map {|stats|stats[:player_name]}
+    binding.pry
+  end
   puts "#{name}, with #{points} points, scored the most points in the game"
 end
 
